9c5de02864264e99b0d0966a7e2ce8793d994209
sanjay26 <sanjay@okrasolar.com>
2020-04-04 14:05:20 +0530

    classes, constructor and super

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var mountains = ["Everest", "K2", "Kilimanjaro"]
-var mountainsJapan = ["Fuji"]
+class Holiday {
+    constructor (destination, days) {
+        this.destination = destination
+        this.days = days
+    }
 
-var everything =  {
-    name: "thing",
-    doIt: "do it"
+    info () {
+        return `${this.destination} wil take ${this.days} days.`
+    }
 }
 
-var pretty = {
-    fuck: "one"
+class Expiditions extends Holiday {
+    constructor (destination, days, gear) {
+        super(destination, days)
+        this.gear = gear
+    }
+
+    info() {
+        return super.info() + `with ${this.gear}`
+    }
 }
 
-var allMountains = [...mountains, ...mountainsJapan]
-var doEver = {...everything, ...pretty}
+const trip = new Holiday("Bangalore, India",15)
+const tripWithGear = new Expiditions("Bangalore,India", 15, "shoes, shorts")
 
-console.log(allMountains)
-console.log(doEver)
+console.log(trip.info())
+console.log(tripWithGear.info())
