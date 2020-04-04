0821b9fd145545d69b69ceb5f6e3b85cba6f637b
sanjay26 <sanjay@okrasolar.com>
2020-04-04 13:14:38 +0530

    restructuring objects

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var mountains = ["Everest", "Kilimanjaro", "K2"]
-var [first, ...rest] = mountains
+var mountains = ["Everest", "K2", "Kilimanjaro"]
+var mountainsJapan = ["Fuji"]
 
-console.log(first)
-console.log (rest)
+var everything =  {
+    name: "thing",
+    doIt: "do it"
+}
+
+var pretty = {
+    fuck: "one"
+}
+
+var allMountains = [...mountains, ...mountainsJapan]
+var doEver = {...everything, ...pretty}
+
+console.log(allMountains)
+console.log(doEver)
