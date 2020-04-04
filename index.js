14b090749b47eacd8d15c42683cf0bc75049f778
sanjay26 <sanjay@okrasolar.com>
2020-04-04 12:29:36 +0530

    object restructuring

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-/* let [firstMountain] = ['Everest', 'K2', 'kilimanjaro']
-console.log(firstMountain) */
-/* let [,firstMountain] = ['Everest', 'K2', 'kilimanjaro']
-console.log(firstMountain) */
-let [,,firstMountain] = ['Everest', 'K2', 'kilimanjaro']
-console.log(firstMountain)
+var name = "Everest"
+var height = "8848"
+var output = function() {
+    console.log(`Mt. ${name} is ${height}m tall.`)
+}
+
+var adventureClimbing = {name, height, output}
+
+adventureClimbing.output()
